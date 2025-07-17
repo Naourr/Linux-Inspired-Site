3d9da2a0e41f59f6bbf862960944a92a34434dc6
Naourr <princessmarielledepalubos2nd@gmail.com>
2025-07-17 23:16:54 +0800

    Im cooking

---
+++ scripts.js
@@ -0,0 +1,26 @@
+const openbtn = document.querySelector('.open');
+const filescreen = document.querySelector('.filescreen');
+
+const clock = document.querySelector('.clock');
+const folder_area = document.querySelectorAll('.folder-area');
+
+openbtn.addEventListener('click', () => {
+    filescreen.classList.toggle('active');
+    const filescreen_on = filescreen.classList.contains('active');
+    if (filescreen_on) {
+        clock.style.opacity = '0';
+    } else {
+        clock.style.opacity = '1';
+    }
+});
+
+const folders = document.querySelectorAll('.folder-area');
+folders.forEach(folder => {
+    folder.addEventListener('click', () => {
+        const name = folder.dataset.folder;
+        const target = document.querySelector(`.for-${name}`);
+        if (target) {
+            target.classList.toggle('active');
+        }
+    });
+});
