2b3c25187efe50fffad6717d6597121693b43c34
Naourr <princessmarielledepalubos2nd@gmail.com>
2025-07-18 23:07:23 +0800

    add preload

--- scripts.js
+++ scripts.js
@@ -1,15 +1,10 @@
 const loading = document.querySelector('.loading');
-
 window.addEventListener('load', () => {
     loading.style.opacity = '0';
 });
 
-
-
-
 const big_placeholder = document.querySelector('.big-placeholder');
 const smol_placeholder = document.querySelector('.smol-placeholder');
-
 document.addEventListener('click', () => {
     const big_window = document.querySelector('.content-wrapper');
     if (allChildrenHidden(big_window)) {
@@ -17,23 +12,18 @@ document.addEventListener('click', () => {
     } else {
         big_placeholder.classList.remove('active');
     }
-
     const smol_window1 = document.querySelector('.list-wrapper');
     if (allChildrenHidden(smol_window1)) {
         smol_placeholder.classList.add('active');
     } else {
         smol_placeholder.classList.remove('active');
     }
-    
 });
-
 
 const openbtn = document.querySelector('.open');
 const filescreen = document.querySelector('.filescreen');
-
 const clock = document.querySelector('.clock');
 const folder_area = document.querySelectorAll('.folder-area');
-
 openbtn.addEventListener('click', () => {
     filescreen.classList.toggle('active');
     const filescreen_on = filescreen.classList.contains('active');
@@ -74,27 +64,24 @@ function allChildrenHidden(parent) {
   });
 }
 
-  function updateClock() {
+function updateClock() {
     const now = new Date();
-
     const options = {
-      weekday: 'long',    // e.g., Monday
-      year: 'numeric',
-      month: 'long',      // e.g., July
-      day: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit',
-      hour12: false        // change to false for 24-hour format
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
     };
-
     const formattedTime = now.toLocaleString('en-US', options);
     document.getElementById('clock').textContent = formattedTime;
-  }
+}
 
-  // Run once immediately, then every second
-  updateClock();
-  setInterval(updateClock, 1000);
+updateClock();
+setInterval(updateClock, 1000);
 
 const icons = document.querySelectorAll('.icon');
 const alert = document.querySelector('.alert-wrapper');
