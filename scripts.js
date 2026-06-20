f5df106f5d16728a6fc10e327cf8d2f52289fdab
Naur <princessmarielledepalubos2nd@gmail.com>
2026-06-20 22:42:40 +0800

    kaduling

--- scripts.js
+++ scripts.js
@@ -84,6 +84,25 @@ function showMainPlaceholder() {
     placeholder.className = 'col placeholder active'
     placeholder.innerHTML = `
                     <p>
+                        <span class='zsh'>『TwinkPad-』</span><span>◤</span>~  ▶ <span>figlet -f shadow "ABOUT ME"</span>
+                    </p>
+                    <div class='row'>
+                        <pre class='figlet'>
+ █████╗ ██████╗  ██████╗ ██╗   ██╗████████╗
+██╔══██╗██╔══██╗██╔═══██╗██║   ██║╚══██╔══╝
+███████║██████╔╝██║   ██║██║   ██║   ██║
+██╔══██║██╔══██╗██║   ██║██║   ██║   ██║
+██║  ██║██████╔╝╚██████╔╝╚██████╔╝   ██║
+╚═╝  ╚═╝╚═════╝  ╚═════╝  ╚═════╝    ╚═╝</pre>
+                        <pre class='figlet'>
+███╗   ███╗███████╗
+████╗ ████║██╔════╝
+██╔████╔██║█████╗  
+██║╚██╔╝██║██╔══╝  
+██║ ╚═╝ ██║███████╗
+╚═╝     ╚═╝╚══════╝</pre>
+                    </div>
+                    <p>
                         <span class='zsh'>『TwinkPad-』</span><span>◤</span>~  ▶ <span>fastfetch</span>
                     </p>
                     <div class='row fastfetch'>
@@ -122,20 +141,115 @@ function showMainPlaceholder() {
                             <p><i class="ph ph-palette"></i> <span class="fetch-key">colors</span>   <span class="c-white">●</span> <span class="c-cyan">●</span> <span class="c-pink">●</span> <span class="c-salmon">●</span> <span class="c-purple">●</span> <span class="c-gray">●</span> <span class="c-black">●</span></p>
                         </div>
                     </div>
-                    <p>
-                        <span class='zsh'>『TwinkPad-』</span><span>◤</span>~  ▶ <span>Insert edgy ahhh placeholder</span><br>
-                        <br>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore itaque, molestias, saepe at tempore earum officiis veniam, 
-                        consequatur distinctio atque facere autem accusantium fugiat eveniet accusamus similique voluptas doloremque tenetur?
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore itaque, molestias, saepe at tempore earum officiis veniam, 
-                        consequatur distinctio atque facere autem accusantium fugiat eveniet accusamus similique voluptas doloremque tenetur?<br>
-                        <br>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore itaque, molestias, saepe at tempore earum officiis veniam, 
-                        consequatur distinctio atque facere autem accusantium fugiat eveniet accusamus similique voluptas doloremque tenetur?<br>
-                        <br>
-                        I will fix this later. Take inspo from neocities and pinterest. PROMISE YOULL FIX THIS.
-
-                    </p>`
+                    <p><span class='zsh'>『TwinkPad-』</span><span>◤</span>~  ▶ <span class="cmd-text">cat intro.md</span></p>
+                    <div class="terminal-session">
+                        <div class="bio-content">
+                            <p class="accent-intro">Hello, I am Princess Marielle P. Depalubos, 2nd year BSCS student and DOST scholar at Cavite State University - Main Campus.</p>
+                            <p>
+                                I dont like talking about myself so lorem ay wait. Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                                officia, dolorem officiis error fuga quod sequi quidem assumenda iusto consequuntur voluptatibus explicabo.
+                            </p>      
+                            <p class="social-links">
+                                <span class="ln-label">Social Links:</span>
+                                <a href="#" target="_blank"><i class="ph ph-github-logo"></i> github</a> | 
+                                <a href="#" target="_blank"><i class="ph ph-facebook-logo"></i> facebook</a> | 
+                                <a href="#" target="_blank"><i class="ph ph-youtube-logo"></i> youtube</a> | 
+                                <a href="#" target="_blank"><i class="ph ph-game-controller"></i> itch.io</a> | 
+                                <a href="#" target="_blank"><i class="ph ph-discord-logo"></i> discord</a>
+                            </p>
+                        </div>
+                    </div>
+                    <p><span class='zsh'>『TwinkPad-』</span><span>◤</span>~  ▶ <span class="cmd-text">pacman -S cs-skills --noconfirm</span></p>
+                    <div class="terminal-session">
+                        <div class="pacman-skills-matrix">
+                            <div class="pacman-row">
+                                <div class="pkg-meta">
+                                    <span class="pkg-name">renpy-engine</span>
+                                    <span class="pkg-stats">5.7 MiB / 14.2 MiB (40%)</span>
+                                </div>
+                                <div class="pacman-bar-wrapper">
+                                    <span class="pacman-bar">[████████████████████c································]</span>
+                                </div>
+                            </div>
+                            <div class="pacman-row">
+                                <div class="pkg-meta">
+                                    <span class="pkg-name">web-stack-meta (html/css/js)</span>
+                                    <span class="pkg-stats">2.9 MiB / 8.4 MiB (35%)</span>
+                                </div>
+                                <div class="pacman-bar-wrapper">
+                                    <span class="pacman-bar">[██████████████████c··································]</span>
+                                </div>
+                            </div>
+                            <div class="pacman-row">
+                                <div class="pkg-meta">
+                                    <span class="pkg-name">linux-hardened-kernel</span>
+                                    <span class="pkg-stats">36.0 MiB / 120.0 MiB (30%)</span>
+                                </div>
+                                <div class="pacman-bar-wrapper">
+                                    <span class="pacman-bar">[███████████████c·····································]</span>
+                                </div>
+                            </div>
+                            <div class="pacman-row">
+                                <div class="pkg-meta">
+                                    <span class="pkg-name">python3</span>
+                                    <span class="pkg-stats">7.0 MiB / 28.1 MiB (25%)</span>
+                                </div>
+                                <div class="pacman-bar-wrapper">
+                                    <span class="pacman-bar">[████████████c········································]</span>
+                                </div>
+                            </div>
+                            <div class="pacman-row">
+                                <div class="pkg-meta">
+                                    <span class="pkg-name">openjdk-java-environment</span>
+                                    <span class="pkg-stats">6.9 MiB / 34.7 MiB (20%)</span>
+                                </div>
+                                <div class="pacman-bar-wrapper">
+                                    <span class="pacman-bar">[██████████c··········································]</span>
+                                </div>
+                            </div>
+                            <div class="pacman-row">
+                                <div class="pkg-meta">
+                                    <span class="pkg-name">godot-core-bin</span>
+                                    <span class="pkg-stats">6.8 MiB / 45.8 MiB (15%)</span>
+                                </div>
+                                <div class="pacman-bar-wrapper">
+                                    <span class="pacman-bar">[███████c·············································]</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <p><span class='zsh'>『TwinkPad-』</span><span>◤</span>~  ▶ <span class="cmd-text">cat history.log</span></p>
+                    <div class="terminal-session">
+                        <div class="bio-content">
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam itaque ex odit nemo similique blanditiis vitae sapiente, 
+                                officia, dolorem officiis error fuga quod sequi quidem assumenda iusto consequuntur voluptatibus explicabo.
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam itaque ex odit nemo similique blanditiis vitae sapiente, 
+                                officia, dolorem officiis error fuga quod sequi quidem assumenda iusto consequuntur voluptatibus explicabo.<br>
+                                <br>
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam itaque ex odit nemo similique blanditiis vitae sapiente, 
+                                officia, dolorem officiis error fuga quod sequi quidem assumenda iusto consequuntur voluptatibus explicabo.
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam itaque ex odit nemo similique blanditiis vitae sapiente, 
+                                officia, dolorem officiis error fuga quod sequi quidem assumenda iusto consequuntur voluptatibus explicabo.
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam itaque ex odit nemo similique blanditiis vitae sapiente.
+                            </p>
+                        </div>
+                    </div>
+                    <p><span class='zsh'>『TwinkPad-』</span><span>◤</span>~  ▶ <span class="cmd-text">task status --current</span></p>
+                    <div class="terminal-session">
+                        <div class="bio-content">
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam itaque ex odit nemo similique blanditiis vitae sapiente, 
+                                officia, dolorem officiis error fuga quod sequi quidem assumenda iusto consequuntur voluptatibus explicabo.
+                            </p>
+                            <ul class="terminal-list">
+                                <li><span class="list-bullet">▶</span> <span class="list-category">[cs]</span> Refining this portfolio site and making it responsive.</li>
+                                <li><span class="list-bullet">▶</span> <span class="list-category">[cs]</span> After finishing that, try to study three.js so I can help my brother out with his plans for his own portfolio site.</li>
+                                <li><span class="list-bullet">▶</span> <span class="list-category">[life]</span> Physically and mentally recovering from last sem, fixing my sleep schedule, and building back the habit of eating 3 times a day lmao.</li>
+                                <li><span class="list-bullet">▶</span> <span class="list-category">[life]</span> Trying to workout again, grow my hair, find my type of clothes. Idk I just cant focus on higher things when im so bothered by looking this chopped all the time wah.</li>
+                            </ul>
+                        </div>
+                    </div>`
     mainStage.appendChild(placeholder)
 }
 function showListPlaceholder() {
