c7f4cf20d70138621d86cfda306b93e9f492b438
Naur <princessmarielledepalubos2nd@gmail.com>
2026-06-21 20:46:58 +0800

    finalize about contents

--- about.js
+++ about.js
@@ -54,16 +54,20 @@ export const mainPlaceholderTemplate = `
         <p><i class="ph ph-hard-drive"></i> <span class="fetch-key">disk:</span> 31.17 GiB / 95.56 GiB (33%) - ext4</p>
         <p><i class="ph ph-cpu"></i> <span class="fetch-key">memory:</span> 2.23 GiB / 15.47 GiB (14%)</p>
         <p><i class="ph ph-wifi-high"></i> <span class="fetch-key">network:</span> 192.168.1.7/24 (wlan0)</p>
-        <p><i class="ph ph-palette"></i> <span class="fetch-key">colors</span>   <span class="c-white">●</span> <span class="c-cyan">●</span> <span class="c-pink">●</span> <span class="c-salmon">●</span> <span class="c-purple">●</span> <span class="c-gray">●</span> <span class="c-black">●</span></p>
+        <p><i class="ph ph-palette"></i> <span class="fetch-key">colors</span>   
+        <span class="c-white">●</span> <span class="c-cyan">●</span> <span class="c-pink">●</span> 
+        <span class="c-salmon">●</span> <span class="c-purple">●</span> <span class="c-gray">●</span> 
+        <span class="c-black">●</span></p>
     </div>
 </div>
 <p><span class='zsh'>『TwinkPad-』</span><span>◤</span>~  ▶ <span class="cmd-text">cat intro.md</span></p>
 <div class="terminal-session">
     <div class="bio-content">
-        <p class="accent-intro">Hello, I am Princess Marielle P. Depalubos, 2nd year BSCS student and DOST scholar at Cavite State University - Main Campus.</p>
+        <p class="accent-intro">
+            Hello, I am Princess Marielle P. Depalubos, 2nd year BSCS student and DOST scholar at Cavite State University - Main Campus.
+        </p>
         <p>
-            I dont like talking about myself so lorem ay wait. Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-            officia, dolorem officiis error fuga quod sequi quidem assumenda iusto consequuntur voluptatibus explicabo.
+            I made this site to showcase the projects ive finished so far, mostly related to Computer Science. And just to flex really ahehehe
         </p>      
         <p class="social-links">
             <span class="ln-label">Social Links:</span>
@@ -133,36 +137,82 @@ export const mainPlaceholderTemplate = `
             </div>
         </div>
     </div>
-</div>
-<p><span class='zsh'>『TwinkPad-』</span><span>◤</span>~  ▶ <span class="cmd-text">cat history.log</span></p>
-<div class="terminal-session">
-    <div class="bio-content">
-        <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam itaque ex odit nemo similique blanditiis vitae sapiente, 
-            officia, dolorem officiis error fuga quod sequi quidem assumenda iusto consequuntur voluptatibus explicabo.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam itaque ex odit nemo similique blanditiis vitae sapiente, 
-            officia, dolorem officiis error fuga quod sequi quidem assumenda iusto consequuntur voluptatibus explicabo.<br>
-            <br>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam itaque ex odit nemo similique blanditiis vitae sapiente, 
-            officia, dolorem officiis error fuga quod sequi quidem assumenda iusto consequuntur voluptatibus explicabo.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam itaque ex odit nemo similique blanditiis vitae sapiente, 
-            officia, dolorem officiis error fuga quod sequi quidem assumenda iusto consequuntur voluptatibus explicabo.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam itaque ex odit nemo similique blanditiis vitae sapiente.
-        </p>
-    </div>
-</div>
+</div>   
+<p><span class='zsh'>『TwinkPad-』</span><span>◤</span>~  ▶ <span class="cmd-text">log --phase G12-renpy_and_CS </span></p>
+<p>
+    Back in G12 during a 2-week sem break, my brother introduced me to RenPy and we just spontaneously agreed to start a project 
+    together. It was the first time I worked on creating something with a computer aside from assignments for school, and 
+    the first time I was able to collaborate with my brother on something technical. It went really well and we both enjoyed 
+    it. It was so unserious that I didn't feel any pressure, just really focused on finishing it even if it didn't turn out 
+    amazing because we were both completely new to it. We finished it in a week. The story revolved around my current friend 
+    group in senior high school, so they were really amused when we showed it to them. After that, I soon had to think about 
+    college programs. I've never thought about those kinds of stuff for even a minute in my life by then, so I just searched up 
+    on YouTube "whats the best college programs" and at the time it said CS. Remembering how productive I felt working on that 
+    RenPy game, it was obvious, either CS, IT, or CpE. And I was just too scared of the more serious sounding courses so, 
+    lmao.
+</p>
+
+<p><span class='zsh'>『TwinkPad-』</span><span>◤</span>~  ▶ <span class="cmd-text">log --phase SUMMER-web_and_game_dev </span></p>
+<p>
+    We graduated by April, and had 4 months of absolute freedom. A friend from SHS who took ICT (I took STEM) 
+    sent some projects they did at school to our gc and I just felt the immediate need to one up them lol, so I figured 
+    Ill be taking CS, so why not start with HTML/CSS/JS? 35 repositories later, realized I'm just bad at design, so maybe I won't 
+    specialize in web dev unless someone designs for me I guess. I also tried Harvard's CS50. I watched the Scratch lecture, made a mini-game, 
+    and then forgot to come back to it lol. Then I tried Godot, but it was too hard as I didn't even know Python basics then.
+</p>
+
+<p><span class='zsh'>『TwinkPad-』</span><span>◤</span>~  ▶ <span class="cmd-text">log --phase SUMMER-linux_ricing </span></p>
+<p>
+    During those 4 months I also tried installing Linux on an old ThinkPad X220 that was passed down to me. It was my first laptop 
+    so I got really excited to customize it. First I tried ZorinOS, but during the installation, the drive got corrupted and 
+    I troubleshooted it for the whole day. That was the first time I felt that level of stress lmaoo poor child. The windows partition 
+    was irrecoverable and I thought I bricked the thing, but turns out I could just fully format the drive and start off clean. 
+    So yay. I liked how cool and custom it looked, so soon after that I tried installing something Arch-based just so I can say 
+    I use arch btw. I tried EndeavourOS + Hyprland. The tiling windows looked hella sick so I endured all the debugging and 
+    tedious config shenanigans, probably wasting a whole month on just tweaking minor stuff. But it was worth it and I really 
+    like how my Linux partition looks now, even if I still use my Windows partition more often. Ngl I only got into Linux for 
+    the aesthetics.
+</p>
+
+<p><span class='zsh'>『TwinkPad-』</span><span>◤</span>~  ▶ <span class="cmd-text">log --phase COLLEGE-rut </span></p>
+<p>
+    Once college started, we learned Python first semester and Java second semester. But honestly, when it started, I wasn't 
+    able to continue progressing in anything at all, both in CS and life. Everything just hit a wall and I went down a bit of a spiral. 
+    I haven't made cool projects I'm proud of with Python or Java yet because it's just more convenient to make graphics with HTML/CSS/JS 
+    instead of learning to do it in Python or Java. And I need to have graphics with my projects because how else would I be able to show them off to my 
+    friends hahah. I use that as motivation too often.. Now I feel kind of directionless, but it should be alright I just need 
+    to learn the fundamentals while building projects I can show off to get something sustainable going, because project-based 
+    learning is definitely the only way anything will stick to my head. But yeah, before I specialize, I really have to focus on fundamentals first. Heck, 
+    I haven't even studied DSA so I really have to asap, maybe after I finish pending projects. About specializations, I've heard 
+    Cyber Security and AI/Machine Learning are the most in-demand ones rn so I should probably try those too after I learn 
+    fundamentals. Ahh its getting obvious, I just need to finish learning programming fundamentals already rahhh.
+</p>
+
 <p><span class='zsh'>『TwinkPad-』</span><span>◤</span>~  ▶ <span class="cmd-text">task status --current</span></p>
 <div class="terminal-session">
     <div class="bio-content">
         <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam itaque ex odit nemo similique blanditiis vitae sapiente, 
-            officia, dolorem officiis error fuga quod sequi quidem assumenda iusto consequuntur voluptatibus explicabo.
+            My goals/priorities/fixations currently: 
         </p>
         <ul class="terminal-list">
-            <li><span class="list-bullet">▶</span> <span class="list-category">[cs]</span> Refining this portfolio site and making it responsive.</li>
-            <li><span class="list-bullet">▶</span> <span class="list-category">[cs]</span> After finishing that, try to study three.js so I can help my brother out with his plans for his own portfolio site.</li>
-            <li><span class="list-bullet">▶</span> <span class="list-category">[life]</span> Physically and mentally recovering from last sem, fixing my sleep schedule, and building back the habit of eating 3 times a day lmao.</li>
-            <li><span class="list-bullet">▶</span> <span class="list-category">[life]</span> Trying to workout again, grow my hair, find my type of clothes. Idk I just cant focus on higher things when im so bothered by looking this chopped all the time wah.</li>
+            <li><span class="list-bullet">▶</span> <span class="list-category">[cs]</span> 
+                Finish this sites refactor and redesign.
+            </li>
+            <li><span class="list-bullet">▶</span> <span class="list-category">[life]</span> 
+                Break bad habits: eat 3 times a day, get out of bed before noon, less doomscrolling.
+            </li>
+            <li><span class="list-bullet">▶</span> <span class="list-category">[life]</span> 
+                Start good habits: workout, get back to journaling and guitar, try new ones like reading. 
+            </li>
+            <li><span class="list-bullet">▶</span> <span class="list-category">[life]</span> 
+                Try to figure out why, ya know, why we keep doing what we do.
+            </li>
+            <li><span class="list-bullet">▶</span> <span class="list-category">[cs]</span> 
+                Try to study three.js to help with my brothers portfolio site.
+            </li>
+            <li><span class="list-bullet">▶</span> <span class="list-category">[cs]</span> 
+                Start studying programming fundamentals.
+            </li>
         </ul>
     </div>
 </div>`
